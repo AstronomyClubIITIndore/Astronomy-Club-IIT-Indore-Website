782accc28be3a981781789d0abd98ee1c540c251
Jaisurya <jaisuryak11@gmail.com>
2023-05-28 14:28:09 +0530

    deployyyy

--- MainProject/settings.py
+++ MainProject/settings.py
@@ -15,8 +15,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 # import environ
-import dotenv
-dotenv.read_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 from pathlib import Path
 
